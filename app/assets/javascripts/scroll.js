5a9879094336323ef63a2d6bec051a4b1bbc03d2
Shuo Chen <nebaries@gmail.com>
2013-07-27 03:43:19 +0100

    use a diff scroll script

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,9 +1,9 @@
 function EasyPeasyParallax() {
-	scrollPos = $(this).scrollTop();
-	$('#bannertext').css({
-		'margin-top': (scrollPos/4)+"px",
-		'opacity': 1-(scrollPos/250)
-	});
+  scrollPos = $(this).scrollTop();
+  $('#bannertext').css({
+   'margin-top': (scrollPos/4)+"px",
+   'opacity': 1-(scrollPos/250)
+  });
 }
 
 $(document).ready(function() {
@@ -14,49 +14,18 @@ $(document).ready(function() {
 		EasyPeasyParallax();
 	});
   
-  function filterPath(string) {
-  return string
-    .replace(/^\//,'')
-    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-    .replace(/\/$/,'');
-  }
-  var locationPath = filterPath(location.pathname);
-  var scrollElem = scrollableElement('html', 'body');
- 
-  $('a[href*=#]').each(function() {
-    var thisPath = filterPath(this.pathname) || locationPath;
-    if (  locationPath == thisPath
-    && (location.hostname == this.hostname || !this.hostname)
-    && this.hash.replace(/#/,'') ) {
-      var $target = $(this.hash), target = this.hash;
-      if (target) {
-        var targetOffset = $target.offset().top;
-        $(this).click(function(event) {
-          event.preventDefault();
-          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
-            location.hash = target;
-          });
-        });
-      }
+  
+  $(".scroll").click(function(event){
+    event.preventDefault();
+    //calculate destination place
+    var dest=0;
+    if($(this.hash).offset().top > $(document).height()-$(window).height()){
+        dest=$(document).height()-$(window).height();
+    }else{
+        dest=$(this.hash).offset().top;
     }
+    //go to destination
+    $('html,body').animate({scrollTop:dest}, 500);
   });
- 
-  // use the first element that is "scrollable"
-  function scrollableElement(els) {
-    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-      var el = arguments[i],
-          $scrollElement = $(el);
-      if ($scrollElement.scrollTop()> 0) {
-        return el;
-      } else {
-        $scrollElement.scrollTop(1);
-        var isScrollable = $scrollElement.scrollTop()> 0;
-        $scrollElement.scrollTop(0);
-        if (isScrollable) {
-          return el;
-        }
-      }
-    }
-    return [];
-  }
+
 });
