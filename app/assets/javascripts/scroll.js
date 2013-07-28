ff3abbc3d84e299c4ad07beffb34068309eb076b
Shuo Chen <nebaries@gmail.com>
2013-07-28 01:37:17 +0100

    style changes

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,31 +1,24 @@
 function EasyPeasyParallax() {
   scrollPos = $(this).scrollTop();
   $('#bannertext').css({
-   'margin-top': (scrollPos/4)+"px",
-   'opacity': Math.max(1-(scrollPos/300), 0)
+   'margin-top': (scrollPos/5)+"px",
+  });
+  $('#banner').css({
+    'opacity': Math.max(1-(scrollPos/400), 0)
   });
 }
 
 $(document).ready(function() {
-  $(document).on('touchmove', function() {
-    EasyPeasyParallax();
-  });
   $(window).scroll(function() {
-		EasyPeasyParallax();
+    if (!('ontouchstart' in window))
+		  EasyPeasyParallax();
 	});
   
-  
   $(".scroll").click(function(event){
-    event.preventDefault();
-    //calculate destination place
-    var dest=0;
-    if($(this.hash).offset().top > $(document).height()-$(window).height()){
-        dest=$(document).height()-$(window).height();
-    }else{
-        dest=$(this.hash).offset().top;
-    }
-    //go to destination
-    $('html,body').animate({scrollTop:dest}, 500);
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
   });
 
 });
